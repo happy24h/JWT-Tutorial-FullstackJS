7e09bba7d20b157d262e802fd0cfc915f381b9d3
@anhnv99 <vietanhhappy99@gmail.com>
2022-11-07 21:40:31 +0700

    delete user

--- practice-backend/index.js
+++ practice-backend/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const authRoute = require("./routes/auth");
+const userRoute = require("./routes/user");
 const app = express();
 
 dotenv.config();
@@ -17,6 +18,7 @@ app.use(express.json());
 
 // ROUTERS
 app.use("/v1/auth", authRoute);
+app.use("/v1/user", userRoute);
 
 app.listen(8000, () => {
   console.log("Server is running");
