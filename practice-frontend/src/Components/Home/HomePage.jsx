39ea8e44cf6d52f1bc7525d43a74e4f6641fdeb1
@anhnv99 <vietanhhappy99@gmail.com>
2022-11-10 20:29:38 +0700

    update

---
+++ practice-frontend/src/Components/Home/HomePage.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getAllUsers, deleteUser } from "../../redux/apiRequest";
+import "./home.css";
+
+const HomePage = () => {
+
+  const user = useSelector((state) => state.auth.login?.currentUser);
+  // optional chaining
+  // ternary operator
+  const userList = useSelector((state) => state.users.users?.allUsers);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+
+  useEffect(() => {
+    if(!user) {
+      navigate("/login")
+    }
+    if(user?.accessToken) {
+      getAllUsers(user?.accessToken, dispatch);
+    }
+    // getAllUsers(user?.accessToken, dispatch);
+
+  }, [])
+
+  const handleDelete = (id) => {
+    deleteUser(user?.accessToken, dispatch, id);
+
+  }
+  return (
+    <main className="home-container">
+      <div className="home-title">User List</div>
+      <div className="home-role">
+        {`Your role: ${user?.admin ? `Admin` : `User`}`}
+      </div>
+      <div className="home-userlist">
+        {userList?.map((user) => {
+          return (
+            <div className="user-container">
+              <div className="home-user">{user.username}</div>
+              <div className="delete-user" onClick={() => handleDelete(user._id)}> Delete </div>
+            </div>
+          );
+        })}
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
