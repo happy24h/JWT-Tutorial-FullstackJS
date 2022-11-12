77f89719e8400f65a6fe236e38b45263bbf72e9f
@anhnv99 <vietanhhappy99@gmail.com>
2022-11-12 11:23:27 +0700

    update practice

---
+++ practice-jwt-token/practice-frontend/src/Components/Home/HomePage.jsx
@@ -0,0 +1,57 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getAllUsers, deleteUser } from "../../redux/apiRequest";
+import "./home.css";
+
+const HomePage = () => {
+
+  // optional chaining
+  // ternary operator
+  const user = useSelector((state) => state.auth.login?.currentUser);
+  const userList = useSelector((state) => state.users.users?.allUsers);
+  const msg = useSelector((state) => state.users?.msg);
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  
+
+  useEffect(() => {
+    if(!user) {
+      navigate("/login")
+    }
+    if(user?.accessToken) {
+      getAllUsers(user?.accessToken, dispatch);
+    }
+    // getAllUsers(user?.accessToken, dispatch);
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const handleDelete = (id) => {
+    deleteUser(user?.accessToken, dispatch, id);
+
+  }
+  return (
+    <main className="home-container">
+      <div className="home-title">User List</div>
+      <div className="home-role">
+        {`Your role: ${user?.admin ? `Admin` : `User`}`}
+      </div>
+      <div className="home-userlist">
+        {userList?.map((user) => {
+          return (
+            <div className="user-container">
+              <div className="home-user">{user.username}</div>
+              <div className="delete-user" onClick={() => handleDelete(user._id)}> Delete </div>
+            </div>
+          );
+        })}
+      </div>
+      <div className="errorMsg">{msg}</div>
+    </main>
+  );
+};
+
+export default HomePage;
---
+++ practice-jwt-token/practice-frontend/src/redux/apiRequest.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import {
+  loginStart,
+  loginSuccess,
+  loginFailed,
+  registerStart,
+  registerSuccess,
+  registerFailed,
+} from "./authSlice";
+import {
+  deleteUserFailed,
+  deleteUserStart,
+  deleteUserSuccess,
+  getUsersFailed,
+  getUsersStart,
+  getUsersSuccess,
+} from "./userSlice";
+
+export const loginUser = async (user, dispatch, navigate) => {
+  dispatch(loginStart());
+
+  try {
+    const res = await axios.post("/v1/auth/login", user);
+    dispatch(loginSuccess(res.data));
+    navigate("/");
+  } catch (err) {
+    dispatch(loginFailed());
+  }
+};
+
+export const registerUser = async (user, dispatch, navigate) => {
+  dispatch(registerStart());
+  try {
+    await axios.post("/v1/auth/register", user);
+    dispatch(registerSuccess());
+    navigate("/login");
+  } catch (err) {
+    dispatch(registerFailed());
+  }
+};
+
+export const getAllUsers = async (accessToken, dispatch) => {
+  dispatch(getUsersStart());
+  try {
+    const res = await axios.get("/v1/user", {
+      headers: { token: `Bearer ${accessToken}` },
+    });
+    dispatch(getUsersSuccess(res.data));
+  } catch (err) {
+    dispatch(getUsersFailed());
+  }
+};
+
+export const deleteUser = async (accessToken, dispatch, id) => {
+  dispatch(deleteUserStart());
+  try {
+    const res = await axios.delete("/v1/user/" + id, {
+      headers: { token: `Bearer ${accessToken}` },
+    });
+    dispatch(deleteUserSuccess(res.data));
+  } catch (err) {
+    dispatch(deleteUserFailed(err.response.data));
+  }
+};
